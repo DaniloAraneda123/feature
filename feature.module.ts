15fe91e826fe7227e49649cbd302ac635b7a19ca
Danilo Araneda <danilo@alert2gain.com>
2022-06-08 10:02:26 -0400

    plantilla

---
+++ feature.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FeatureRoutingModule } from './feature.routing';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FeatureRoutingModule,
+    SharedModule
+  ]
+})
+export class FeatureModule { }
